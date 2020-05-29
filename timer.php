d088f796b544b40a13f3bd3653fea071f483529a
Kay McKelly <frecklestars@gmail.com>
2020-05-29 13:19:14 -0700

    Timer done with PKC sponsors, tweak to orange theme

--- timer.php
+++ timer.php
@@ -48,8 +48,6 @@
         </h1>
         <div id="clockdiv" class="d-flex w-50 mt-4 justify-content-center">
           <h3 class="pt-2 mr-4">Time until session starts:</h3>
-          <!-- <div class="d-none px-1"><span class="days"></span></div>
-          <div class="d-none px-1"><span class="hours"></span>:</div> -->
           <div class="px-1 w-25">
             <span class="minutes"></span>:<span class="seconds"></span>
           </div>
@@ -65,84 +63,29 @@
         <section class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="images/sponsors/eurocrypt2020/tiilogo.png" class="d-block w-50 mx-auto" alt="Technology Innovation Institute">
+              <img src="images/sponsors/pkc2020/sicsa-logo.jpg" class="d-block w-50 mx-auto" alt="The Scottish Informatics and Computer Science Alliance (SICSA)">
               <h4 class="text-center mt-3">
-                Diamond Sponsor
+                Local Sponsor
+              </h4>
+            </div>
+            <div class="carousel-item">
+              <img src="images/sponsors/pkc2020/nexus-logo.jpg" class="d-block w-50 mx-auto" alt="Cybersecurity Nexus">
+              <h4 class="text-center mt-3">
+                Local Sponsor
               </h4>
             </div>
 
             <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/calibra.png" class="d-block w-50 mx-auto" alt="Calibra">
-              <h4 class="text-center mt-3">
-                Gold Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/VisaResearch.png" class="d-block w-50 mx-auto" alt="Visa Research">
-              <h4 class="text-center mt-3">
-                Gold Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/google.png" class="d-block w-50 mx-auto" alt="Google">
+              <img src="images/sponsors/pkc2020/iohk.svg" class="d-block w-50 mx-auto" alt="IOHK">
               <h4 class="text-center mt-3">
                 Gold Sponsor
               </h4>
             </div>
 
             <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/cloudflare.png" class="d-block w-50 mx-auto" alt="Cloudflare">
+              <img src="images/sponsors/pkc2020/dfinity.png" class="d-block w-50 mx-auto" alt="Dfinity">
               <h4 class="text-center mt-3">
                 Silver Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/IBM-Research-logo.png" class="d-block w-50 mx-auto" alt="IBM Research">
-              <h4 class="text-center mt-3">
-                Silver Sponsor
-              </h4>
-            </div>
-
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/CEAlogo.png" class="d-block w-50 mx-auto" alt="CEA">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/crx.png" class="d-block w-50 mx-auto" alt="CryptoExperts">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/hardwear_logo.png" class="d-block w-50 mx-auto bg-dark" alt="hardwear.io">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/Logo-Intrinsic-ID_238x40.png" class="d-block w-50 mx-auto" alt="Intrinsic ID">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/pqshield.jpg" class="d-block w-50 mx-auto" alt="PQShield">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/rambus.png" class="d-block w-50 mx-auto" alt="Rambus">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
-              </h4>
-            </div>
-            <div class="carousel-item">
-              <img src="images/sponsors/eurocrypt2020/PlatON.png" class="d-block w-50 mx-auto" alt="PlatON">
-              <h4 class="text-center mt-3">
-                Bronze Sponsor
               </h4>
             </div>
           </div>
@@ -150,77 +93,6 @@
       </div>
     </main>
 
-    <!-- <div id="page" class="mt-0">
-      <div id="headerGrad">
-        <div class="row">
-          <div class="col-4">
-            <span class="px-2">Eurocrypt 2020</span>
-          </div>
-          <div class="col-8">
-            <div id="clockdiv" class="d-none d-flex flex-row">
-              <span class="mr-5">Time until session starts:</span>
-              <div class="d-none px-1"><span class="days"></span></div>
-              <div class="d-none px-1"><span class="hours"></span>:</div>
-              <div class="px-1 w-25">
-                <span class="minutes"></span>:<span class="seconds"></span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <h2 class="text-center mt-3 mb-3">
-        Thank you to our sponsors
-      </h2>
-      <div id="sponsorslideshow" class="w-75 mx-auto">
-        <div class="carousel slide" data-ride="carousel" data-interval="10000">
-          <div class="carousel-item active text-center">
-            <img src="images/sponsors/eurocrypt2020/tiilogo.png" class="img-fluid w-50 mx-auto">
-            <h4 class="text-center">Diamond Sponsor</h4>
-          </div>
-          <div class="carousel-item">
-            <h4 class="text-center">Gold Sponsor</h4>
-            <img src="images/sponsors/eurocrypt2020/calibra.png" class="img-fluid align-middle">
-          </div>
-          <div class="carousel-item">
-            <h4 class="text-center">Gold Sponsor</h4>
-            <img src="images/sponsors/eurocrypt2020/VisaResearch.png" class="mt-5 d-block img-fluid w-50 mx-auto">
-          </div>
-          <div class="carousel-item">
-            <h4 class="mt-4 text-center">Gold Sponsor</h4>
-            <img src="images/sponsors/eurocrypt2020/google.png" class="mt-5 d-block img-fluid w-50 mx-auto">
-          </div>
-          <div class="carousel-item">
-            <h4 class="text-center">Silver Sponsor</h4>
-            <img src="images/sponsors/eurocrypt2020/cloudflare.png" class="d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <h4 class="mt-5 text-center">Silver Sponsor</h4>
-            <img src="images/sponsors/eurocrypt2020/IBM-Research-logo.png" class="mt-5 d-block img-fluid w-75 mx-auto">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/CEAlogo.png" class="d-block img-fluid mx-auto w-50">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/crx.png" class="d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <img style="background-color:#000000;" src="images/sponsors/eurocrypt2020/hardwear_logo.png" class="mt-5 d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/Logo-Intrinsic-ID_238x40.png" class="mt-5 d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/pqshield.jpg" class="d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/rambus.png" class="d-block img-fluid mx-auto w-100">
-          </div>
-          <div class="carousel-item">
-            <img src="images/sponsors/eurocrypt2020/PlatON.png" class="d-block img-fluid mx-auto w-25">
-          </div>
-        </div>
-      </div>
-    </div> -->
     <!-- NOTE: you need this, even though you don't display it, as it includes jQuery -->
     <?php include "includes/footer.php"; ?>
 <script>
@@ -249,8 +121,6 @@
    function updateClock() {
      var t = getTimeRemaining(endtime);
 
-//     daysSpan.innerText = t.days;
-//     hoursSpan.innerText = ('0' + t.hours).slice(-2);
      minutesSpan.innerText = ('0' + t.minutes).slice(-2);
      secondsSpan.innerText = ('0' + t.seconds).slice(-2);
 
@@ -263,8 +133,6 @@
    updateClock();
    var timeinterval = setInterval(updateClock, 1000);
  }
-// var deadline = new Date(Date.parse('2020-05-11T13:00:00Z'));
- // initializeClock('clockdiv', deadline);
 
 </script>
 
